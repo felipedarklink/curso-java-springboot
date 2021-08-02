521ba2c3ccf3087689f5c96a120a8ac435b6c853
Felipe Silva <felipeosilva@hotmail.com>
2021-08-02 12:15:52 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/felipeosilva/course1/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.felipeosilva.course1.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipeosilva.course1.entities.Order;
+import com.felipeosilva.course1.entities.User;
+import com.felipeosilva.course1.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
